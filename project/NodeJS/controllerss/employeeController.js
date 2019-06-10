ac93057e50f9caab0f9e666d35c387328f11480e
Sankalp100 <sankalpgour123@gmail.com>
2019-06-11 00:16:27 +0530

    backend done

--- project/NodeJS/controllerss/employeeController.js
+++ project/NodeJS/controllerss/employeeController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Employee } = require('../models/employees');
 
@@ -7,7 +8,17 @@ var { Employee } = require('../models/employees');
 router.get('/', (req, res) => {
     Employee.find((err, docs) => {
         if (!err) { res.send(docs); }
-        else { console.log('Error in Retriving :' + JSON.stringify(err, undefined, 2));}
+        else { console.log('Error in Retriving :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No Record with given id: ${req.params.id}`);
+
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Employee Reteiving :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
@@ -19,8 +30,33 @@ router.post('/', (req, res) => {
         salary: req.body.salary
     });
     emp.save((err, doc) => {
-        if (!err) { res.send(doc);}
-        else { console.log('Error in Employee Save :' + JSON.stringify(err, undefined, 2));}
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Employee Save :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.put('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No Record with given id: ${req.params.id}`);
+    var emp = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Employee Update :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No Record with given id: ${req.params.id}`);
+
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Employee Delete :' + JSON.stringify(err, undefined, 2)); } 
     });
 });
 
