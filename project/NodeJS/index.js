6fbbe3e5fccf34a1b5f1ae31cf9d64de34249443
Sankalp100 <sankalpgour123@gmail.com>
2019-06-10 23:47:19 +0530

    schema

--- project/NodeJS/index.js
+++ project/NodeJS/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db');
+var employeeController = require('./controllerss/employeeController');
 
 var app = express();
 app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('Sever started at port : 3000'));
+
+app.use('/employees', employeeController);
--- project/NodeJS/models/employees.js
+++ project/NodeJS/models/employees.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 var Employee = mongoose.model('Employee', {
-    name: { type:String },
+    name: { type: String },
     position: { type: String },
     office: { type: String },
     salary: { type: Number }
 });
 
-module.exports = Employee ;
+module.exports = { Employee };
+
