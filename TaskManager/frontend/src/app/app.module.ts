4b353f48b52b0a26b3f01d0c9b6ccff4ae94c832
Sankalp100 <sankalpgour123@gmail.com>
2019-06-18 00:44:01 +0530

    new-task

--- TaskManager/frontend/src/app/app.module.ts
+++ TaskManager/frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskViewComponent,
-    NewListComponent
+    NewListComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
