1309d36bc8f13649bbecd231b907d5fa6a192fa3
Sankalp100 <sankalpgour123@gmail.com>
2019-06-23 23:38:12 +0530

    user model

---
+++ TaskManager/api/db/Models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    sessions: [{
+        token: {
+            type: String,
+            required: true
+        },
+        expireAt: {
+            type: Number,
+            required: true
+        }
+    }]
+})
