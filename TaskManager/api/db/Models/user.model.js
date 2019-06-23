63213b3b82ea6c4e3eb78ce1fd9895b56c644ec4
Sankalp100 <sankalpgour123@gmail.com>
2019-06-24 00:00:20 +0530

    lodesh and jsonwebtoken

--- TaskManager/api/db/Models/user.model.js
+++ TaskManager/api/db/Models/user.model.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const _ = require("lodesh");
+const jwt = require("jsonwebtoken");
+
+// JWT Secret 
+const jwtSecret = "51778657246321226641fsdklafjasdkljfsklfjd7148924065";
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -23,4 +28,25 @@ const UserSchema = new mongoose.Schema({
             required: true
         }
     }]
-})
+});
+
+// Instance methods
+
+UserSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.omit(userObject, ['password','session']);
+}
+
+UserSchema.methods.generateAccessAuthToken = function(){
+    const user = this;
+    return new Promise((resolve, reject) => {
+        // Create the JSON web Token and return that
+        jwt.sign({_id: user._id.toHexString() }, jwtSecret, { expiresIn: "15m"}, (err, token) => {
+            if(!err) {
+                
+            }
+        })
+    })
+}
