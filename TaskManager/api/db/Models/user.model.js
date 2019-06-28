061254a386af8c3450cd4b013e07c83423f4bec2
Sankalp100 <sankalpgour123@gmail.com>
2019-06-28 23:47:08 +0530

    password

--- TaskManager/api/db/Models/user.model.js
+++ TaskManager/api/db/Models/user.model.js
@@ -90,6 +90,18 @@ UserSchema.static.findByIdAndToken = function(_id, token) {
 
 UserSchema.statics.findByCredentials = function(email, password){
     let User = this;
+    return User.findOne({ email }).then((user) => {
+        if(!user) return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) resolve(user);
+                else {
+                    reject();
+                }
+            })
+        })
+    })
 }
 
 
@@ -106,6 +118,8 @@ UserSchema.pre('save', function (next) {
                 next();
             })
         })
+    } else {
+        next();
     }
 });
 
